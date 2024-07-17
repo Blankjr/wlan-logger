9e7d65b71cf5a91fa0f54652be520ce1d7fb6890
Blankjr <blankj.me@gmail.com>
2024-07-17 15:48:45 +0200

    + last commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,9 @@
-import { Text, View } from "react-native";
+import SelectDestination from "@/components/userinput/SelectDestination";
+import { View } from "react-native";
+import { Button, Text } from "react-native-paper";
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+import {ThemeContext} from '../config/Theme/context';
+import {MoonMeetDarkTheme} from '../config/Theme/theme';
 
 export default function Index() {
   return (
@@ -9,7 +14,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text variant="displayLarge">Edit app/index.tsx to edit this screen.</Text>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+  <SelectDestination></SelectDestination>
     </View>
   );
 }
