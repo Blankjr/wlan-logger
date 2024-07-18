e11bb9ac3846b8b32610ad1dae5f3cbaf4e8a632
Blankjr <blankj.me@gmail.com>
2024-07-18 19:33:33 +0200

    customTheme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,18 @@ import { DarkTheme } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { DefaultTheme, PaperProvider } from "react-native-paper";
 
-const theme = {
-  
-}
+const customTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'blue', // Color of the selected tab icon and text
+    accent: 'red', // Color of the navigation bar background
+  },
+};
 
 export default function RootLayout() {
   return (
-    <PaperProvider theme={DefaultTheme}>
+    <PaperProvider theme={customTheme}>
       <Stack>
         <Stack.Screen name="index" />
       </Stack>
