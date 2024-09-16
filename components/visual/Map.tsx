eda1a8820f5338d34eab7b2a7969b75ebcf12aca
Blankjr <blankj.me@gmail.com>
2024-09-16 14:42:03 +0200

    feat/wifi-data

--- components/visual/Map.tsx
+++ components/visual/Map.tsx
@@ -1,19 +1,31 @@
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import NetInfo from '@react-native-community/netinfo';
-
-NetInfo.fetch().then(state => {
-  console.log('Connection type', state.type);
-  console.log('Is connected?', state.isConnected);
-  console.log(state.details)
-});
+import {useNetInfo} from "@react-native-community/netinfo";
 
 const Map = ({ floorNumber, roomNumber }) => {
+  const netInfo = useNetInfo();
+  console.log(netInfo.details)
   return (
     <View style={styles.container}>
       <Text>Selected Floor: {floorNumber}</Text>
       <Text>Room Number: {roomNumber}</Text>
       {/* Map implementation */}
+      <Text>Type: {netInfo.type}</Text>
+      <Text>Is Connected? {netInfo.isConnected?.toString()}</Text>
+      {/* Check if the connection is Wi-Fi before accessing details */}
+      {netInfo.type === 'wifi' && netInfo.details ? (
+        <View>
+          <Text>Strength: {netInfo.details.strength}</Text>
+          <Text>IP Address: {netInfo.details.ipAddress}</Text>
+          <Text>Link Speed: {netInfo.details.linkSpeed}</Text>
+          <Text>bssid: {netInfo.details.bssid}</Text>
+          <Text>ssid: {netInfo.details.ssid}</Text>
+          <Text>subnet: {netInfo.details.subnet}</Text>
+        </View>
+      ) : (
+        <Text>No Wi-Fi details available.</Text>
+      )}
+
     </View>
   );
 };
