0637316152bc566ecc15248dac180d1d582f2137
Blankjr <blankj.me@gmail.com>
2024-09-16 14:42:22 +0200

    app.config.js

--- app.config.js
+++ app.config.js
@@ -1,9 +1,17 @@
-const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_DEV = process.env.APP_VARIANT === "development";
+const IS_BETA = process.env.APP_VARIANT === "beta";
+const IS_PREVIEW = process.env.APP_VARIANT === "preview";
+
+const APP_NAME = "Freibewegen";
 
 export default {
   expo: {
-    name: IS_DEV? "navigation-client (DEV)" : "navigation-client",
-    slug: "navigation-client",
+    name: IS_DEV
+    ? `[DEV] ${APP_NAME}`
+    : IS_PREVIEW
+      ? `[PREVIEW] ${APP_NAME}`
+      : (APP_NAME),
+    slug: APP_NAME,
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
@@ -22,7 +30,12 @@ export default {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff",
       },
-      package: "com.blankjr.navigationclient",
+      // package: "com.blankjr.navigationclient",
+      package:
+      IS_DEV
+        ? "com.blankjr.navigationclient.dev"
+        : "com.blankjr.navigationclient",
+      // useNextNotificationsApi: IS_DEV,  // Add this line to differentiate behavior
     },
     web: {
       bundler: "metro",
@@ -40,7 +53,7 @@ export default {
         origin: false,
       },
       eas: {
-        projectId: "94338853-85a1-4d95-a593-1cdc7899da05",
+        projectId: "53469d35-baad-4722-9cc9-6c1f986bd03e",
       },
     },
   },
