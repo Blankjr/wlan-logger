8a637aa1f522bf508065407014030c1e96ed7d38
Blankjr <blankj.me@gmail.com>
2024-12-30 13:17:36 +0100

    fix: support native statusbar changes even with hotreloads, dont change colors of native ui parts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 import { DarkTheme } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { DefaultTheme, PaperProvider } from "react-native-paper";
+import { StatusBar } from "expo-status-bar";
+import { useColorScheme, View } from 'react-native';
 
 const customTheme = {
   ...DefaultTheme,
@@ -12,8 +14,19 @@ const customTheme = {
 };
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+
   return (
     <PaperProvider theme={customTheme}>
+      <View style={{
+        backgroundColor: isDarkMode ? '#000000' : '#FFFFFF',
+        paddingTop: 30
+      }}>
+        <StatusBar 
+          style={isDarkMode ? 'light' : 'dark'}
+        />
+      </View>
       <Stack>
         <Stack.Screen name="index" 
         options={{
